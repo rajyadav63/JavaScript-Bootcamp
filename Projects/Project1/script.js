b8dcdb1ce4229e571ead0896f06615940a305046
raj36git <vector36razz@gmail.com>
2025-01-13 15:07:02 +0530

    Project 1 finished

--- Projects/Project1/script.js
+++ Projects/Project1/script.js
@@ -1,25 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const todoInput = document.getElementById('todo-input');
+    const buttonClick = document.getElementById('btn')
+    const todoList = document.getElementById('todo-list')
 
-const todoInput = document.getElementById('todo-input');
-const buttonClick = document.getElementById('btn')
-const todoList = document.getElementById('todo-list')
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [] //return data if already loaded or will return empty array, 
 
-let tasks = []
-todoInput.addEventListener('click', function () {
-    let addTask = todoInput.value.trim();
-    if (addTask === '') return;
+    tasks.forEach(task => renderData(task));
 
-    const newTask = {
-        id: Date.now(),
-        text: addTask,
-        completed: false
+    todoInput.addEventListener('click', function () {
+        let addTask = todoInput.value.trim();
+        if (addTask === '') return;
+
+        const newTask = {
+            id: Date.now(),
+            text: addTask,
+            completed: false
+        }
+        tasks.push(newTask);
+        saveToLocal();
+        todoInput.value = '' //clear after adding
+        console.log(tasks);
+    })
+
+    //retrive the data
+    function renderData(task) {
+        const li = document.createElement('id');
+        li.className = 'task-item';
+        li.setAttribute("data-id", task.id)
+        li.innerHTML = `
+        <span>${task.text}</span>
+        <button class="delete-button">delete</button>
+        `
+        todoList.appendChild(li)
+
     }
-    tasks.push(newTask);
-    saveToLocal();
-    todoInput.value = '' //clear after adding
-    console.log(tasks);
-})
 
-//save data to local storage
-function saveToLocal() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+    //save data to local storage
+    function saveToLocal() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+})
