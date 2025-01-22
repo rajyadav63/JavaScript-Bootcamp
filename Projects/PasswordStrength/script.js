8a42a4ab723c1b6cc0780a0edf41ff4addd4095f
rajyadav <vector36razz@gmail.com>
2025-01-22 20:35:37 +0530

    strength color changed

--- Projects/PasswordStrength/script.js
+++ Projects/PasswordStrength/script.js
@@ -14,11 +14,14 @@ password.addEventListener('input', function () {
     if (password.value.length <= 4) {
         strength.innerText = `Weak`
         password.style.borderColor = "red"
+        strength.style.color = 'red'
     } else if (password.value.length > 4 && password.value.length < 8) {
         strength.innerText = `Medium`
         password.style.borderColor = "yellow"
+        strength.style.color = 'yellow'
     } else if (password.value.length >= 8) {
         strength.innerText = `Strong`
-        password.style.borderColor = "green"
+        password.style.borderColor = "#11f011"
+        strength.style.color = '#11f011'
     }
 });
